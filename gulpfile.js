028bcc6eeb13af7ad1a08f5ad6589c98d0e334ea
Giovanni Mendoza <mendozagioo@gmail.com>
2017-07-12 18:43:14 -0500

    Se modificó la configuración para la compilación de estilos

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,20 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 // Bootstrap scss source
-var bootstrapSass = {
-  in: './node_modules/bootstrap-sass/'
-};
+var bootstrapSass = './bootstrap/stylesheets/';
 
 var sassOpts = {
   outputStyle: 'compressed',
   precison: 3,
-  errLogToConsole: true,
-  includePaths: [bootstrapSass.in + 'assets/stylesheets']
+  errLogToConsole: true
 }
 
 gulp.task('sass', function() {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src(bootstrapSass + '**/**/*.scss')
     .pipe(sass(sassOpts).on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch(bootstrapSass + '**/**/*.scss', ['sass']);
 });
